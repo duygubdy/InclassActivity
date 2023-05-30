07f6746552431dbfb5a0f3b5263b26520b5e8990
duygubdy <duygubdy@gmail.com>
2023-05-30 21:23:34 +0300

    Activity10 added

---
+++ Activity10/Activity10_Slider/slider.js
@@ -0,0 +1,18 @@
+
+
+  $(document).ready(function() {
+	$("#slider").bxSlider({
+	  auto: true,
+	  captions: true,
+	  slideWidth: 250,
+	  slideMargin: 10,
+	  randomStart: true,
+	  minSlides: 1,
+	  maxSlides: 1,
+	  moveSlides: 1,
+	  pause: 3000,
+	  pager: "#pager",
+	  pagerType: 'short'
+	});
+  });
+
